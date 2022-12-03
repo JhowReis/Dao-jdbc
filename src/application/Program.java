de98e85730c467718674a52476e03200b83dc134
jonathan reis <jonathan.linkedin2019@gmail.com>
2022-12-02 22:40:33 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
 
+
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(2,"jonathan","joanthan@teste.com",new Date(),3.000,obj);
+
+        System.out.println(seller);
     }
 }
